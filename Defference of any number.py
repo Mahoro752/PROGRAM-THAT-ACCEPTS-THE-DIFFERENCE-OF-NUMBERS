044cf38a47a70df64d9ed05a11dd3ee25a4bc5cb
Mahoro752 <61754973+Mahoro752@users.noreply.github.com>
2020-03-09 20:14:22 +0300

    Add files via upload

---
+++ Defference of any number.py
@@ -0,0 +1,5 @@
+first_number = int(input("Enter first Number: "))
+second_number = int(input("Enter Second number: "))
+x = first_number - second_number
+print()
+print("The Diffrence between first and second number is: ", x)
